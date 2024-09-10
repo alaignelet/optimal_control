3d257825ba349813e4286560d18b27b0b34c09e2
alaignelet <alexis.laignelet@gmail.com>
2024-09-10 19:44:06 +0100

    add inputs

---
+++ inputs/non_linear_true_solution/semi_lagrangian/nonlinear2dpi.m
@@ -0,0 +1,109 @@
+%function [aold,Vold,time,timevi,countsub,countvi,Values,Valuesvi] = accvdppi(deltax,ncont,mmfile)
+
+deltax=0.025; %gridspacing
+h=.05*deltax;
+ncont=200;
+
+x=1;%bounds to solve in a square [-x x][-x x]
+
+xnode=-x:deltax:x;
+[X,Y]=ndgrid(xnode);
+
+lambda=.001; % discount factor, needs to be small to mimic the HJB equation.
+dim=length(xnode);
+
+Vold=ones(dim,dim);
+inside=ones(dim,dim,ncont);
+inside(2:dim-1,2:dim-1,:)=0;
+inside1=squeeze(inside(:,:,1));
+Vold(X.^2+Y.^2==0)=0;
+Tg=Vold;
+
+
+
+aold=zeros(dim,dim); %initial control
+
+count=0;
+subc=0;
+cb=7; % control bounds to look for a control variables in [-cb cb]
+Acont=-cb:(2*cb/(ncont-1)):cb;
+
+
+Varr=zeros(dim,dim);
+Disc=zeros(dim,dim);
+
+T=zeros(dim,dim,ncont);
+Valuesvi=zeros(dim,dim,dim);
+
+
+
+Values=zeros(dim,dim,dim);
+diff1=1;
+count=1;
+tol=1/5*deltax^2;
+
+gamma=0.5;
+p=2;
+Disc=0.5*(X.^2+Y.^2);
+
+tic
+while(diff1>tol)
+VI=Vold;
+diff=1;
+
+
+arrx=X+h*Y;
+arry=Y+h*(X.^3+aold);
+countsub(count)=0;
+
+while (diff>tol)
+    Varr=interpn(X,Y,Vold,arrx,arry,'linear',5);
+    Vnew=exp(-lambda*h)*Varr+h*(Disc+gamma*abs(aold).^p);
+    Vnew(Tg==0)=0;
+    diff=max(max(abs(Vnew-Vold))); 
+    Vold=Vnew;   
+    countsub(count)=countsub(count)+1;
+end
+
+Values(:,:,count)=Vold;
+
+    for k=1:ncont
+        arrx=X+h*Y;
+        arry=Y+h*(X.^3+Acont(k));
+        Varr=interpn(X,Y,Vold,arrx,arry,'linear',1);
+        T(:,:,k)=exp(-lambda*h)*Varr+h*(Disc+gamma*abs(Acont(k)).^p);
+    end
+    T(inside==1)=5;
+    [~,donde]=min(T,[],3);
+    
+ for i=1:ncont
+     aold(donde==i)=Acont(i);
+ end
+    
+    diff1=max(max(abs(VI-Vold)))
+    count=count+1;
+end
+
+time=toc;
+Values(:,:,[count:dim])=[];
+figure
+surf(Vold)
+figure
+surf(aold)
+
+
+
+%SDRE solution
+Pi=@(x1,x2)[((x1^4 + 1)^(1/2)*(2*(x1^4 + 1)^(1/2) + 2*x1^2 + 1)^(1/2))/2 (x1^4 + 1)^(1/2)/2 + x1^2/2;(x1^4 + 1)^(1/2)/2 + x1^2/2 (2*(x1^4 + 1)^(1/2) + 2*x1^2 + 1)^(1/2)/2];
+
+Xv=X(:);
+Yv=Y(:);
+Vex=[];
+for i=1:length(Xv)
+    Vex(i)=[Xv(i) Yv(i)]*Pi(Xv(i),Yv(i))*[Xv(i) Yv(i)]';
+end
+
+Vex=reshape(Vex,size(X));
+
+figure
+surf(abs(Vold-Vex))
